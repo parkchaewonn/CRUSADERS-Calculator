2864822dd468643a6a4166e22508c0cc0d1a58dc
chrisdevoir <86392788+chrisdevoir@users.noreply.github.com>
2021-07-02 20:53:13 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,2 +1,2 @@
 #This branch is for the unit test
-#codes for the unittest will be uploaded soon after the finishing the codes for the Logic and GUI 
+#codes for the unittest will be uploaded after finishing the codes for the Logic and GUI 
