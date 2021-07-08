71c9125347c8abb70a97b6268a01f48cb600d04f
parkchaewonn <86221121+parkchaewonn@users.noreply.github.com>
2021-07-08 22:33:46 +0800

    Finalizing the logic's code

--- logic.py
+++ logic.py
@@ -15,27 +15,7 @@ def Calculate(self):
     except ZeroDivisionError:
         self.displayResult("Zero Division Error")
         self.function = ""
-
-def displayResult(self, value):
-    self.input.delete(0, END)
-    self.input.insert(0, value)
-
-def Clear(self):
-    self.function = ""
-    self.input.delete(0, END)
-    self.input.insert(0, "0")
-
-def DeleteNum(self):
-    self.function = self.input.get()[:-1]
-    self.input.delete(0, END)
-    self.input.insert(0, self.function)
-
-def ExpNum(self):
-    self.function = math.pow(float(self.input.get()), 2)
-    self.input.delete(0, END)
-    self.input.insert(0, self.function)
         
-def SqrtNum(self):
-    self.function = math.sqrt(float(self.input.get()))
-    self.input.delete(0, END)
-    self.input.insert(0, self.function)
+    except OverflowError:
+        self.displayResult("Math Error")
+        self.function = ""
