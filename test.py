32bd86f09693058f3cae87ccf5910e00ce111377
RichmarAgum <86400344+RichmarAgum@users.noreply.github.com>
2021-07-08 23:07:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,18 @@ class Test(unittest.TestCase):
         g = Logic()
         g.input = "1/1"
         self.assertEqual(g.Calculate(), 1 )
-
+    def test_1(self):
+        g = Logic()
+        g.input = "2/1"
+        self.assertEqual(g.Calculate(), 2)
+    def test_5(self):
+        g = Logic()
+        g.input = "2+ 2+2"
+        self.assertEqual(g.Calculate(), 6)
+    def test_5312(self):
+        g = Logic()
+        g.input = "2 -5 "
+        self.assertEqual(g.Calculate(), -3)
 
 if __name__ == "__main__":
     unittest.main()
